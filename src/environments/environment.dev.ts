c1cbe9f229002a071b5a427ebad7b418e6e8454d
Francesco Mannarino <franm1003@gmail.com>
2025-07-12 03:37:03 +0200

    add environment template

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    source: "developer",
+    firebase: {
+        apiKey: "",
+        authDomain: "",
+        projectId: "surveys-hub",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
+    },
+    apiUrl: 'http://localhost:8080/api'
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    source: "production",
+    firebase: {
+        apiKey: "",
+        authDomain: "",
+        projectId: "surveys-hub",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: ""
+    },
+    apiUrl: 'https://surveys-hub-be.europe-west1.run.app/api'
+};
